82cc0c7fe4a79ccb70cc017a8cff9302974ff70f
Leandro <leandroviscolungo@gmail.com>
2025-10-29 21:52:59 -0300

    reinicio

--- drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-
-export default defineConfig({
-  schema: './lib/drizzle/schema.ts',
-  out: './drizzle',
-  connectionString: process.env.DATABASE_URL || ''
-} as any)
